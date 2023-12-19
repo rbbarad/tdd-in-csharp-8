34071010fffd63b1a913069816f1f0bceed4ba89
Rishi Barad <rbbarad@amazon.com>
2023-12-19 13:39:05 -0500

    Create a Red Unit Test

---
+++ DeskBooker/DeskBooker.Core.Tests/Processor/DeskBookingRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace DeskBooker.Core.Processor
+{
+    internal class DeskBookingRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ DeskBooker/DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessor.cs
@@ -0,0 +1,14 @@
+﻿namespace DeskBooker.Core.Processor
+{
+    internal class DeskBookingRequestProcessor
+    {
+        public DeskBookingRequestProcessor()
+        {
+        }
+
+        internal DeskBookingResult BookDesk(DeskBookingRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
